7497053c2771ac63a9eb4e01bae5a9ff1d2d975b
localhostd3veloper <gautamanand022@gmail.com>
2023-01-21 23:06:41 +0530

    tailwind updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Body from "./components/Body";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="h-screen bg-slate-100 flex">
+      <Sidebar />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
