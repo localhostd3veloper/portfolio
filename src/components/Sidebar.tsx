64dcd09843d65a8ecc211e68b58b976aab2aac43
localhostd3veloper <gautamanand022@gmail.com>
2023-01-21 23:06:04 +0530

    add: sideBar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,20 @@
+const options = ["home", "about", "projects", "experience", "contact"];
+
+function Sidebar() {
+  return (
+    <nav className="w-1/4 bg-gray-800 text-slate-300 flex justify-center items-center">
+      <ul className="flex flex-col gap-5 text-2xl">
+        {options.map((option) => (
+          <li
+            key={option}
+            className="uppercase font-semibold cursor-pointer hover:text-slate-700 duration-200"
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Sidebar;
