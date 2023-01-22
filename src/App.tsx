801d64170c58cdd61291a052c7bc08ca6645f512
localhostd3veloper <gautamanand022@gmail.com>
2023-01-22 22:16:16 +0530

    react-transition-group

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,33 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import { Route, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 function App() {
   return (
-    <div className="h-screen bg-slate-100 flex">
+    <div className="h-screen flex bg-[#101010]">
       <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Body />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/experience">
-          <Experience />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={300} classNames="fade">
+              <Switch>
+                <Route exact path="/">
+                  <Body />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/experience">
+                  <Experience />
+                </Route>
+                <Route path="/projects">
+                  <Projects />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </div>
   );
 }
