dfe3b6cda5e84f3e1896bb0a9f55e3d65d5196aa
localhostd3veloper <gautamanand022@gmail.com>
2023-01-22 18:13:35 +0530

    router: updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import Body from "./components/Body";
 import Sidebar from "./components/Sidebar";
-
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="h-screen bg-slate-100 flex">
       <Sidebar />
-      <Body />
+      <Switch>
+        <Route exact path="/">
+          <Body />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/experience">
+          <Experience />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+      </Switch>
     </div>
   );
 }
