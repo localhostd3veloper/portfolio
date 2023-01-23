b4bb38043309e4d1957619a9018f13178a908a44
localhostd3veloper <gautamanand022@gmail.com>
2023-01-23 15:24:05 +0530

    add: URLS

--- src/static/static-data.ts
+++ src/static/static-data.ts
@@ -100,18 +100,21 @@ export const experienceItems = [
     cardDetailedText: `
     Replicated complex Figma designs using various frameworks on react. Collaborating with cross-functional teams to define, design, and ship new features. Practiced various cyber security concepts and types of attacks on virtual
     machines and docker containers.`,
+    url: "https://www.treacletech.com/",
   },
   {
     title: "July 2022 - Sep 2022", // Date
     cardTitle: "Software Developer Intern", // Job Title
     cardSubtitle: "Listnr, Inc.", // Company Name
     cardDetailedText: `Replicating complex UI's from the wireframes and parallelly writing logic for the same. Improved the lighthouse score by reducing the overall bundle size of the webpack which made the platform 30% faster. Co-authored pull requests on GitHub with other engineers on the team`,
+    url: "https://www.listnr.tech/",
   },
   {
     title: "Feb 2022 - Jul 2022", // Date
     cardTitle: "Full Stack Developer & Chatbot Expert", // Job Title
     cardSubtitle: "Botosynthesis®", // Company Name
     cardDetailedText: `Writing bug-free, modular and reusable code for developing Full Stack Applications. Supervising Interns in developing AI-based chatbots on various platforms like - WhatsApp, Facebook Messenger, Instagram, etc. Using the Botosynthesis.ai platform, I created rule-based chatbots with extra automation features`,
+    url: "https://www.botosynthesis.ai/",
   },
   {
     title: "Oct 2020 - Jan 2022",
@@ -122,5 +125,6 @@ export const experienceItems = [
     Developed complex conversational APIs and Algorithms. I've also played a major
     role at the the backend of a few conventional AI platforms.
     Working on projects and simultaneously creating SDLC for the same`,
+    url: "https://www.botosynthesis.ai/",
   },
 ];
