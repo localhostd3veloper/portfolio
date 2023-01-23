62ecaf30f4c1ebd4d3cec082337bf598749d0bfe
localhostd3veloper <gautamanand022@gmail.com>
2023-01-23 11:58:01 +0530

    padding updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,25 @@
 function About() {
   return (
-    <div className="page px-32 py-9 text-gray-300 bg-[#101010]">
+    <div className="page px-32 pr-60 pt-12 text-gray-300 bg-[#101010] flex flex-col justify-center">
       <h1 className="text-4xl font-semibold">About</h1>
       <video autoPlay className="w-full h-1/3 filter grayscale">
         <source src={require("../assets/about-video.mp4")} type="video/mp4" />
       </video>
-      <p className="mt-5 text-3xl font-semibold">Hello there 👋</p>
-      <p className="text-xl">
-        I'm a full-stack developer with a passion for creating beautiful and
-        functional web applications. I'm currently (Till August 2023) studying
-        in my final year of a Bachelor Technology in Computer Science at{" "}
-        <a
-          href="https://gits.ac.in"
-          className="underline underline-offset-4 hover:text-blue-500"
-        >
-          Geetanjali Institute of Technical Studies
-        </a>
-        , Udaipur
-      </p>
+      <section>
+        <p className="mt-5 text-3xl font-semibold">Hello there 👋</p>
+        <p className="text-xl">
+          I'm a full-stack developer with a passion for creating beautiful and
+          functional web applications. I'm currently (Till August 2023) studying
+          in my final year of a Bachelor Technology in Computer Science at{" "}
+          <a
+            href="https://gits.ac.in"
+            className="underline underline-offset-4 hover:text-blue-500 duration-300"
+          >
+            Geetanjali Institute of Technical Studies
+          </a>
+          , Udaipur
+        </p>{" "}
+      </section>
     </div>
   );
 }
