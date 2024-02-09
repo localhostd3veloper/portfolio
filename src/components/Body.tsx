4fe90eb0199d1faee57323d9a91dfabc09e5a7a8
localhostd3veloper <gautamanand022@gmail.com>
2024-02-09 16:30:17 +0530

    update: body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -8,14 +8,14 @@ import {
 
 function Body() {
   return (
-    <div className="sm:w-3/4 h-screen flex md:flex-row flex-col px-10 sm:px-0 justify-center items-center gap-6 sm:gap-12 overflow-hidden">
-      <div className="bg-main bg-cover bg-center h-72 w-72 rounded-full fucking-animation  hover:shadow-2xl duration-300 filter grayscale"></div>
+    <div className="h-dvh flex md:flex-row flex-col md:px-10 px-4 justify-center items-center gap-6 sm:gap-12 overflow-hidden">
+      <div className="bg-main bg-cover bg-center h-72 w-72 rounded-full fucking-animation hover:shadow-2xl duration-300"></div>
       <div className="flex flex-col">
-        <span className="uppercase text-[3.5rem] font-bold text-white hover:tracking-wide hover:scale-105 duration-500 ease-in-out ">
+        <p className="uppercase text-3xl font-bold hover:tracking-wide hover:scale-105 duration-500 ease-in-out leading-none">
           Gautam Anand
-        </span>
-        <div className="text-3xl flex gap-2 text-white">
-          I am a{" "}
+        </p>
+        <div className="text-2xl md:text-3xl flex gap-2 flex-nowrap">
+          I am a
           <Typewriter
             options={{
               autoStart: true,
@@ -25,30 +25,30 @@ function Body() {
             }}
           />
         </div>
-        <div className="flex gap-5 mt-6">
+        <div className="flex flex-wrap gap-5 mt-6">
           {socialMediaLinks.map((social) => (
             <a
               key={social.name}
               href={social.link}
               target="_blank"
               rel="noreferrer"
-              className="text-xl text-white hover:text-slate-500 duration-300 ease-in-out flex gap-1 items-center"
+              className="text-xl hover:text-slate-500 duration-300 ease-in-out flex-wrap flex gap-1 items-center"
             >
               <i className={`${social.icon} text-4xl sm:text-base `}></i>{" "}
-              <span className="hidden sm:block">{social.name}</span>
+              <p className="hidden sm:block">{social.name}</p>
             </a>
           ))}
         </div>
-        <div className="sm:hidden flex gap-2 underline tracking-tighter">
+        <div className="sm:hidden flex flex-wrap gap-2 font-medium tracking-tighter">
           {sidebarOptions.map(
             (option, index) =>
               index !== 0 && (
                 <Link
                   key={option.name}
                   to={option.link}
-                  className="text-xl text-white hover:text-slate-500 duration-300 ease-in-out flex gap-1 items-center mt-4"
+                  className="text-xl   hover:text-slate-500 duration-300 ease-in-out flex gap-1 items-center mt-4"
                 >
-                  <span className="">{option.name}</span>
+                  <p className="uppercase">{option.name}</p>
                 </Link>
               )
           )}
