c9daf3b55b0bee78a4fd1c1a51bbb19904f15d5c
localhostd3veloper <gautamanand022@gmail.com>
2024-02-15 14:55:59 +0530

    Experience Page Updates

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,36 +1,39 @@
-import { Chrono } from "react-chrono";
-import { Link } from "react-router-dom";
-import { experienceItems } from "../static/static-data";
+import { Chrono } from 'react-chrono';
+import { Link } from 'react-router-dom';
+import { experienceItems } from '../static/static-data';
 
 function Experience() {
   return (
-    <div className="sm:w-3/4 overflow-auto px-3 sm:px-12 pt-6  text-gray-700">
-      <h1 className="text-4xl font-semibold flex justify-between">
-       Work Experience{" "}
+    <div className='sm:w-3/4 overflow-auto px-3 sm:px-12 pt-6  text-gray-700'>
+      <h1 className='text-4xl font-semibold flex justify-between'>
+        Work Experience{' '}
         <Link
-          to="/"
-          className="underline text-blue-300 block ml-auto sm:hidden text-lg"
+          to='/'
+          className='underline text-blue-300 block ml-auto sm:hidden text-lg'
         >
           Home
         </Link>
       </h1>
-      <div  className=" ">
+      <div className=' '>
         <Chrono
-          title="Experience"
+          title='Experience'
           items={experienceItems}
-          mode="VERTICAL_ALTERNATING"
+          mode='VERTICAL_ALTERNATING'
+          scrollable
+          cardHeight={200}
           slideShow
-          slideItemDuration={3000}
-          theme={{
-            primary: "#2e2e2e",
-            secondary: "#f5f5f5",
-            cardForeColor: "#f5f5f5",
-            titleColor: "#f5f5f5",
-            cardBgColor: "#2e2e2e",
+          slideItemDuration={5000}
+          allowDynamicUpdate
+          classNames={{
+            card: 'p-3 divide-y divide-gray-500 rounded-md',
           }}
-          hideControls
+          theme={{
+            cardBgColor: '#d1d5db',
+            titleColorActive: 'blue',
+            secondary: '#d1d5db',
+          }}
           useReadMore
-          cardWidth={400}
+          cardWidth={500}
         />
       </div>
     </div>
