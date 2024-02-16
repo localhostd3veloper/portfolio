10bf3e037f250edfa1e4031297b308c258cd8ced
localhostd3veloper <gautamanand022@gmail.com>
2024-02-16 15:13:49 +0530

    exxperience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,30 +5,30 @@ import TitleWrapper from './TitleWrapper';
 
 function Experience() {
   return (
-    <SectionWrapper id='experience'>
-      <TitleWrapper>
-        Work Experience
-      </TitleWrapper>
-      <div className='w-full'>
+    <SectionWrapper
+      id='experience'
+      className='justify-center items-center gap-4'
+    >
+      <TitleWrapper>Work Experience</TitleWrapper>
+      <div className='w-full flex justify-center items-center md:w-4/5 lg:w-1/2'>
         <Chrono
           title='Experience'
           items={experienceItems}
           mode='HORIZONTAL'
           scrollable
           cardHeight={200}
-          slideShow
-          slideItemDuration={5000}
-          allowDynamicUpdate
           classNames={{
-            card: 'p-3 divide-y divide-gray-500 rounded-md',
+            card: 'p-3 divide-y divide-gray-500 rounded-md shadow',
           }}
+          hideControls
           theme={{
             cardBgColor: '#d1d5db',
             titleColorActive: 'blue',
             secondary: '#d1d5db',
           }}
           useReadMore
-          cardWidth={600}
+          slideItemDuration={3000}
+          slideShow
         />
       </div>
     </SectionWrapper>
