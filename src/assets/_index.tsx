2f876b75f6df05d1504f94603681c8b6d6252a2b
localhostd3veloper <gautamanand022@gmail.com>
2024-02-16 22:04:44 +0530

    agg: images

---
+++ src/assets/_index.tsx
@@ -0,0 +1,27 @@
+import dashboardImg1 from './dashboard-1.png';
+import dashboardImg2 from './dashboard-2.png';
+import landingImg from './landing.png';
+import landingImg2 from './landing-2.png';
+import loginImg from './login.png';
+import registerImg from './register.png';
+import macosXImg from './mac-osx.png';
+import macosXImg2 from './mac-osx-2.png';
+import paperGenImg from './paper-gen.png';
+import ticketlessImg from './ticketless.png';
+import netflixImg from './netflix.png';
+import netflixImg2 from './netflix-2.png';
+
+export {
+  dashboardImg1,
+  dashboardImg2,
+  landingImg,
+  landingImg2,
+  loginImg,
+  registerImg,
+  macosXImg,
+  macosXImg2,
+  paperGenImg,
+  ticketlessImg,
+  netflixImg,
+  netflixImg2,
+};
